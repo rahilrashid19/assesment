8f1676263e40e8d732340db95b4e521c1cf3ca23
Rahil Rashid <rrashid@sparkcognition.com>
2023-12-23 14:34:30 +0530

    Added Pagination

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import useFetchData from "../utils/useFetchData";
 import Card from "./Card";
 import logo from "../assets/logo.png";
+import LoginPage from "./LoginPage";
+import ReactPaginate from "react-paginate";
+import "../../src/paginate.css";
 
 const MainScreen = () => {
   const cardData = useFetchData(
@@ -9,10 +12,41 @@ const MainScreen = () => {
   );
 
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // state variable for logout
+  const cardsPerPage = 9;
 
-  const filteredCards = cardData?.data?.filter((card) =>
+  // Check if cardData is not yet available
+  if (!cardData) {
+    return null;
+  }
+
+  const signOut = () => {
+    setIsLoggedIn(true);
+  };
+
+  // if user is logged in , then i will set isLoggedIn true which will take us to login page again
+  if (isLoggedIn) {
+    return <LoginPage />;
+  }
+
+  const filteredCards = cardData.data?.filter((card) =>
     card.crop_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  // Check if filteredCards is not yet available
+  if (!filteredCards) {
+    return null;
+  }
+
+  const pageCount = Math.ceil(filteredCards.length / cardsPerPage);
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * cardsPerPage;
+  const currentCards = filteredCards.slice(offset, offset + cardsPerPage);
 
   return (
     <div>
@@ -25,14 +59,55 @@ const MainScreen = () => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <div></div>
+        <div>
+          <button
+            onClick={() => {
+              signOut();
+            }}
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Log Out
+          </button>
+        </div>
       </div>
+
+      {/* pagination section */}
 
       <div className="flex flex-col items-center p-4 bg-gray-200">
         <div className="flex flex-wrap justify-between">
-          {filteredCards?.map((card) => (
+          {currentCards.map((card) => (
             <Card {...card} key={card.id} />
           ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: 20,
+            boxSizing: "border-box",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <ReactPaginate
+            previousLabel="Previous"
+            nextLabel="Next"
+            pageCount={pageCount}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            pageLinkClassName="page-link"
+            previousLinkClassName="page-link"
+            nextLinkClassName="page-link"
+            activeClassName={"item active "}
+            breakClassName={"item break-me "}
+            breakLabel={"..."}
+            containerClassName={"pagination"}
+            disabledClassName={"disabled-page"}
+            marginPagesDisplayed={2}
+            nextClassName={"item next "}
+            pageClassName={"item pagination-page "}
+            previousClassName={"item previous"}
+          />
         </div>
       </div>
     </div>
